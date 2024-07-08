ea09b0e44237c0fc9bcd45fadf7414903555a09b
Vipul Malhotra <vipulm124@gmail.com>
2024-07-09 00:52:43 +0530

    updated marke payment complet method

---
+++ src/Libraries/Nop.Services/Payments/RazorpayPaymentSaveRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Nop.Services.Payments;
+
+public partial class RazorpayPaymentSaveRequest
+{
+    public int BabySaffronOrderId { get; set; }
+    public string RazorpayPaymentId { get; set; }
+    
+}
+
