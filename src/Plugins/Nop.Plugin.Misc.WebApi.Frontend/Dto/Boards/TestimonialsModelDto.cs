3283de3850e66ae7028dac35e5c7c411618fee31
Vipul Malhotra <vipulm124@gmail.com>
2024-08-01 01:06:27 +0530

    added missed files

---
+++ src/Plugins/Nop.Plugin.Misc.WebApi.Frontend/Dto/Boards/TestimonialsModelDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Nop.Plugin.Misc.WebApi.Framework.Dto;
+
+namespace Nop.Plugin.Misc.WebApi.Frontend.Dto.Boards; 
+
+public partial class TestimonialsModelDto : ModelWithIdDto
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public string Rating { get; set; }
+}
+
---
+++ src/Presentation/Nop.Web/Models/Boards/TestimonialModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Models.Boards;
+
+public partial record TestimonialModel : BaseNopModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Rating { get; set; }
+
+}
+
